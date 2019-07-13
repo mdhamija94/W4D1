943a107ef52b53dd9dabdcda0a427d0e41f4e729
mdhamija94 <mdhamija@gmail.com>
2019-07-12 19:45:30 -0700

    Initializes KnightPathFinder class

---
+++ skeleton/lib/knight_path_finder.rb
@@ -0,0 +1,27 @@
+require_relative "./00_tree_node.rb"
+
+class KnightPathFinder
+    
+
+
+    def initialize(start_pos)
+        @root_node = PolyTreeNode.new(start_pos)
+        @considered_positions = [start_pos]
+        
+    end
+
+    def build_move_tree
+       #start with root node?
+        @root_node
+    end
+
+    def self.valid_moves(pos)
+
+    end
+
+    def new_move_positions(pos)
+        current_valid_moves = KnightPathFinder.valid_moves(pos)
+        current_valid_moves.reject! { |position| @considered_positions.include?(position) }
+    end
+
+end
